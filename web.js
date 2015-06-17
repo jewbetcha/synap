847144ebb33f6d769e1c57e5a543554294f9d6ef
Coleman Rollins <coleman@ten7.com>
2015-06-17 13:40:48 -0500

    trying things with my package to get it to work on heroku

--- web.js
+++ web.js
@@ -1,4 +1,1 @@
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname + './'));
-app.listen(process.env.PORT || 3000);
+this.server.listen(process.env.PORT || port);
